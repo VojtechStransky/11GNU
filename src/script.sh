39b7e19d987b80e043eadc13e7eb18fd7c866704
Vojtech Stransky <stranskyvo@protonmail.com>
2024-09-17 23:49:54 +0200

    Script modified

--- src/script.sh
+++ src/script.sh
@@ -15,10 +15,10 @@ gain=$(sed -nr '/^#\s*Amplifier\sGain/{s/.*Amplifier\sGain\s*:\s*(([0-9]+.?[0-9]
 frequency=$(sed -nr '/^#\s*Sampling\sFreq.:/{s/.*Sampling\sFreq.\s*:\s*(([0-9]+.?[0-9]+)).*/\1/;p;}' $file)
 
 if [ "$sigma" = true ] ; then
-    ./a.out --number-n=$number --return-sigma --file=$fileDat
+    gnucode --number-n=$number --return-sigma --file=$fileDat
     yLabel="Variance[V^{2}]"
 else
-    ./a.out --number-n=$number --file=$fileDat
+    gnucode --number-n=$number --file=$fileDat
     yLabel="Mean[V]"
 fi
 
